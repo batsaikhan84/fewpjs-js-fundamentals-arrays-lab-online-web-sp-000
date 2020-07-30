f3fd6429f6a68a5688ba01b85b4f93953c16a31e
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-07-30 04:05:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const append = ["Milo", "Otis", "Garfield"], prepend = ["Milo", "Otis", "Garfield"], removeLast = ["Milo", "Otis", "Garfield"], removeFirst = ["Milo", "Otis", "Garfield"]
 
 append.push("Odie")
+prepend.unshift("Odie")
