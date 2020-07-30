5674237f80bd3c83536018464c2594a34a9794b7
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-07-30 04:03:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-const appent, prepend, removeLast, removeFirst
+const append = ["Milo", "Otis", "Garfield"], prepend = ["Milo", "Otis", "Garfield"], removeLast = ["Milo", "Otis", "Garfield"], removeFirst = ["Milo", "Otis", "Garfield"]
